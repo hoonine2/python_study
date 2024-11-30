ad3ada3e9fcafdbe753b117129d257ab36a31e56
jonghoon <hoonine2@gmail.com>
2024-11-30 17:05:45 +0900

    conditional study

---
+++ loop.py
@@ -0,0 +1,15 @@
+# i = 0
+# sum = 0
+
+# for i in range(1, 11):
+#     sum = sum + i
+
+# print(sum)
+
+# while문은 조건식이 True일때 반복함
+
+progress = 0
+
+while progress < 100:
+    progress = progress + 1
+    print(f"{progress}% completed")
---
+++ prompt.py
@@ -0,0 +1,4 @@
+# input("설치를 진행합니까? (y/n): ")
+text = input("출력할 텍스트를 입력해주세요: ")
+
+print(text)
